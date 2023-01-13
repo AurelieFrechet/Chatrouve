fb3dcc19056f8229b19a00de1bb7b1fcf60c0264
Aurélie Fréchet <aurelie.frechet@gmail.com>
2023-01-13 16:30:14 +0100

    organisation unit test sql

--- tests/testthat/test-sql_requests.R
+++ tests/testthat/test-sql_requests.R
@@ -1,63 +1,95 @@
-# USERS ----
+db <- dbConnect(RSQLite::SQLite(), "../data/unit_testing.sqlite")
+
+# CREATE ----
 test_that("create and delete table users", {
-  db <- dbConnect(RSQLite::SQLite(), "../data/unit_testing.sqlite")
-  create_table_users(db)
+  expect_error(create_table_users(db), NA)
   expect_true(dbExistsTable(db, "users"))
-  delete_table_users(db)
-  expect_false(dbExistsTable(db, "users"))
-  dbDisconnect(db)
 })
 
-
-test_that("insert valid values in table users", {
-  db <- dbConnect(RSQLite::SQLite(), "../data/unit_testing.sqlite")
-  create_table_users(db)
-  insert_into_users(
-    db,
-    user_id  = "Maxime",
-    name     = "Maxime",
-    email    = "Maxime@maxime.fr",
-    password = "zouzou"
-  )
-  user <- select_user(db, email = "Maxime@maxime.fr")
-  expect_equal(user$user_id, "Maxime")
-  expect_equal(user$name, "Maxime")
-  expect_equal(user$email, "Maxime@maxime.fr")
-  expect_equal(user$password, "zouzou")
-  
-  expect_true(exists_email(db, email = "Maxime@maxime.fr"))
-  expect_false(exists_email(db, email = "Maxime@chatrouvé."))
-  dbDisconnect(db)
-})
-
-
-
-# CATS ----
 test_that("create and delete table cats", {
-  db <- dbConnect(RSQLite::SQLite(), "../data/unit_testing.sqlite")
-  create_table_cats(db)
+  expect_error(create_table_cats(db), NA)
   expect_true(dbExistsTable(db, "cats"))
-  delete_table_cats(db)
-  expect_false(dbExistsTable(db, "cats"))
-  dbDisconnect(db)
 })
 
-test_that("insert valid values in table cats", {
-  db <- dbConnect(RSQLite::SQLite(), "../data/unit_testing.sqlite")
-  create_table_cats(db)
-  insert_into_cats(
-    db,
-    cat_id       = "TEST01",
-    body_size    = "small",
-    fluffy       = TRUE,
-    coat_pattern = "Tabby",
-    coat_color   = "white",
-    eyes_color   = "green",
-    behavior     = "shy",
-    comments     = "",
-    user_id      = "Maxime"
+test_that("create and delete table locations", {
+  expect_error(create_table_locations(db), NA)
+  expect_true(dbExistsTable(db, "locations"))
+})
+
+# INSERT ----
+test_that("insert values in table users", {
+  expect_error(
+    insert_into_users(
+      db,
+      user_id  = "00000001",
+      name     = "Aurélien",
+      email    = "Aurelien@mangedesfrites.be",
+      password = "loulou"
+    ),
+    NA
   )
-  cats <- select_cats(db, user_id = "Maxime")
+  
+  expect_error(
+    insert_into_users(
+      db,
+      user_id  = "00000002",
+      name     = "Aurélien",
+      email    = "Aurelien@mangedesfrites.be",
+      password = "jaioubliemonmotdepasse"
+    )
+  )
+  
+})
+
+test_that("insert values in table cats", {
+  expect_error(
+    insert_into_cats(
+      db,
+      cat_id       = "TEST01",
+      body_size    = "small",
+      fluffy       = TRUE,
+      coat_pattern = "Tabby",
+      coat_color   = "white",
+      eyes_color   = "green",
+      behavior     = "shy",
+      comments     = "",
+      user_id      = "00000001"
+    ),
+    NA
+  )
+})
+
+test_that("insert values in table locations", {
+  expect_error(
+    insert_into_location(
+      db,
+      cat_id  = "TEST01",
+      seen_at = "2023-01-13 12:20:23 CET",
+      lat     = 29.975519613000223,
+      lng     = 31.137621042180164
+    ),
+    NA
+  )
+  
+})
+
+
+# SELECT ----
+test_that("select  users", {
+  user <- select_user(db, email = "Aurelien@mangedesfrites.be")
+  expect_equal(user$user_id, "00000001")
+  expect_equal(user$name, "Aurélien")
+  expect_equal(user$email, "Aurelien@mangedesfrites.be")
+  expect_equal(user$password, "loulou")
+  
+  
+  empty <- select_user(db, email = "Aurelien@chatrouvé.fr")
+  expect_equal(nrow(empty), 0)
+})
+
+
+test_that("select  cats", {
+  cats <- select_cats(db, user_id = "00000001")
   expect_equal(cats$cat_id, "TEST01")
   expect_equal(cats$body_size, "small")
   expect_equal(cats$fluffy, 1)
@@ -66,16 +98,49 @@ test_that("insert valid values in table cats", {
   expect_equal(cats$eyes_color, "green")
   expect_equal(cats$behavior, "shy")
   expect_equal(cats$comments, "")
-  expect_equal(cats$user_id, "Maxime")
-  dbDisconnect(db)
+  expect_equal(cats$user_id, "00000001")
+  
+  
+  empty <- select_cats(db, user_id = "00000002")
+  expect_equal(nrow(empty), 0)
 })
 
 
-test_that("create and delete table locations", {
-  db <- dbConnect(RSQLite::SQLite(), "../data/unit_testing.sqlite")
-  create_table_locations(db)
-  expect_true(dbExistsTable(db, "locations"))
-  delete_table_locations(db)
+test_that("select location", {
+  location <- select_locations(db, cat_id = "TEST01")
+  expect_equal(location$seen_at, "2023-01-13 12:20:23 CET")
+  expect_equal(location$lat, 29.975519613000223)
+  expect_equal(location$lng, 31.137621042180164)
+  
+  empty <- select_locations(db, cat_id = "TEST02")
+  expect_equal(nrow(empty), 0)
+})
+
+
+
+# EXISTS ----
+test_that("existing users with email", {
+  expect_true(exists_email(db, email = "Aurelien@mangedesfrites.be"))
+  expect_false(exists_email(db, email = "Aurelien@chatrouvé.fr"))
+})
+
+
+# DELETE ----
+test_that("delete table locations", {
+  expect_error(delete_table_locations(db), NA)
   expect_false(dbExistsTable(db, "locations"))
-  dbDisconnect(db)
 })
+
+test_that("delete table cats", {
+  expect_error(delete_table_cats(db), NA)
+  expect_false(dbExistsTable(db, "cats"))
+})
+
+test_that("delete table users", {
+  expect_error(delete_table_users(db), NA)
+  expect_false(dbExistsTable(db, "users"))
+  
+})
+
+
+dbDisconnect(db)
