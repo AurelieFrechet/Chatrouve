b1491dce3f6ed4be4659326c06e9fd48eba441db
Aurélie Fréchet <aurelie.frechet@gmail.com>
2023-06-02 23:27:01 +0200

    add draw cat module

---
+++ inst/app/modules/mod_drawcat.R
@@ -0,0 +1,49 @@
+# UI ----------------------------------------------------------------------
+drawcat_UI <- function(id) {
+  ns <- NS(id)
+  tagList(
+  uiOutput(ns("cat_picture")))
+}
+
+
+
+# SERVER ------------------------------------------------------------------
+
+drawcat_SERVER <- function(id, eyes_color, coat_color) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+    
+    output$cat_picture <- renderUI({
+      readLines("inst/app/www/img/dessin.svg", warn = FALSE)%>%
+        paste(collapse = " ") %>%
+        str_replace_all(pattern = "yellow", replacement = eyes_color) %>% 
+        str_replace_all(pattern = "black",  replacement = coat_color) %>% 
+        HTML()
+      
+    })
+    
+  })
+}
+
+
+
+# Test --------------------------------------------------------------------
+library(shiny)
+library(stringr)
+library(shinyjs)
+
+ui <- fluidPage(useShinyjs(),
+                drawcat_UI("test"),
+                drawcat_UI("test2")
+                )
+
+server <- function(input, output, session) {
+  drawcat_SERVER("test",
+                 coat_color = "#e5aa8c",
+                 eyes_color = "#24d4f9")
+  drawcat_SERVER("test2",
+                 eyes_color = "#e5fa8c",
+                 coat_color = "#f4d3f9")
+}
+
+shinyApp(ui, server)
